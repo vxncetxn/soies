7654cea1a046276df0b027fc577ff9fd80ac9673
Vance Tan <vxncetxn@gmail.com>
2026-06-05 03:07:36 +0800

    Added theme-aware color tokens

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,22 +3,19 @@ import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white px-8 dark:bg-black">
+    <View className="flex-1 items-center justify-center bg-background px-8">
       {/* Heading */}
-      <Text className="mb-3 text-4xl font-extrabold tracking-tight text-gray-800 dark:text-white">
-        🚀 Welcome
-      </Text>
+      <Text className="mb-3 text-4xl font-extrabold tracking-tight text-primary">🚀 Welcome</Text>
 
       {/* Subheading */}
-      <Text className="mb-8 text-center text-xl leading-relaxed text-gray-700 dark:text-white">
+      <Text className="mb-8 text-center text-xl leading-relaxed text-secondary">
         Build beautiful apps with{" "}
         <Text className="font-semibold text-blue-500">Expo (Router) + Uniwind 🔥</Text>
       </Text>
 
       {/* Instruction text */}
-      <Text className="max-w-sm text-center text-base text-gray-600 dark:text-white">
-        Start customizing your app by editing{" "}
-        <Text className="font-semibold text-gray-800 dark:text-white">app/index.tsx</Text>
+      <Text className="max-w-sm text-center text-base text-primary">
+        Start customizing your app by editing <Text className="font-semibold">app/index.tsx</Text>
       </Text>
 
       <StatusBar style="dark" />
